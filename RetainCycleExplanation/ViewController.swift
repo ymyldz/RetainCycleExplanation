f1d12a575cd3f4e240eff10c4cd17d6be788c6d9
ymyldz <yusfmertyildiz@gmail.com>
2023-05-20 15:15:07 +0300

    Initial Commit

---
+++ RetainCycleExplanation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RetainCycleExplanation
+//
+//  Created by Yusuf Mert Yıldız on 20.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
